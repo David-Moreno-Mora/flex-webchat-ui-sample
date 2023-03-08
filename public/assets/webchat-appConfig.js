a3595756d75a2d9bb79173b67d8a49315e8597d6
David-Moreno-Mora <davidmoreno@multiplica.com>
2023-03-08 16:22:02 -0600

    Credentials

---
+++ public/assets/webchat-appConfig.js
@@ -0,0 +1,9 @@
+var appConfig = {
+    //  change the your AccountSid
+    accountSid: "ACc628d92d7709589556457b4304befcb1",
+    // change to your Flex Flow SID
+    flexFlowSid: "FO1cef12486c80972f152341e88c79cf61",
+    colorTheme: {
+        overrides: brandedColors
+    }
+}
